5d5ed2afca50738624fb8e61ea37227d059bd6fc
Stefano.In91 <ste.ince91@gmail.com>
2022-10-24 14:25:40 +0200

    adds html-manipulative js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,30 @@
 "use strict";
 
+// Fizz buzz function
 for ( let i = 1 ; i <= 100 ; i++ ) {
+   // Individuazione lista in html
+   const ul = document.getElementById("list");
+   // Creazione elemento come funzione
+   let createLi = document.createElement("li");
+   
    if (((i % 3) == 0) && ((i % 5) == 0 )) {
-      console.log(`fizzbuzz`);
+      // Multipli 5 e 3, fizz buzz
+      createLi.innerHTML = `fizzbuzz`;
+      ul.append(createLi);
+      createLi.classList.add("fizzbuzz");
    } else if ((i % 3) == 0) {
-      console.log(`fizz`);
+      // Multipli 3 fizz
+      createLi.innerHTML = `fizz`;
+      ul.append(createLi);
+      createLi.classList.add("fizz");
    } else if ((i % 5) == 0) {
-      console.log(`buzz`);
+      // Multipli 5 buzz
+      createLi.innerHTML = `buzz`;
+      ul.append(createLi);
+      createLi.classList.add("buzz");
    } else {
-      console.log(i);
+      // Non multipli i
+      createLi.innerHTML = i;
+      ul.append(createLi);
    }
 }
